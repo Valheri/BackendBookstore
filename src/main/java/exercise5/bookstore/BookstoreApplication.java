83819372d0844f60d842db69ee0a069e3b869d77
Valheri <valtteri.vuokila@gmail.com>
2025-02-01 13:52:34 +0200

    Kirjojen lisäys ja poisto listasta

--- src/main/java/exercise5/bookstore/BookstoreApplication.java
+++ src/main/java/exercise5/bookstore/BookstoreApplication.java
@@ -21,6 +21,7 @@ public class BookstoreApplication {
             repository.save(new Book("Book Title 1", "Author 1", 2001, "ISBN001", 19.99));
             repository.save(new Book("Book Title 2", "Author 2", 2002, "ISBN002", 29.99));
             repository.save(new Book("Book Title 3", "Author 3", 2003, "ISBN003", 39.99));
+            repository.save(new Book("Book Title 4", "Author 4", 2004, "ISBN004", 49.99));
         };
     }
 }
--- src/main/java/exercise5/bookstore/controller/BookController.java
+++ src/main/java/exercise5/bookstore/controller/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import exercise5.bookstore.model.Book;
 import exercise5.bookstore.repository.BookRepository;
 
 @Controller
@@ -23,5 +27,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
