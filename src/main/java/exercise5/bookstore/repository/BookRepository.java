da6d5b0e74d0902dd5d6f7d48afea53c4754a85a
Valheri <valtteri.vuokila@gmail.com>
2025-02-22 17:35:30 +0200

    Enable Spring Data REST

--- src/main/java/exercise5/bookstore/repository/BookRepository.java
+++ src/main/java/exercise5/bookstore/repository/BookRepository.java
@@ -1,8 +1,13 @@
 package exercise5.bookstore.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import exercise5.bookstore.model.Book;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
 }
